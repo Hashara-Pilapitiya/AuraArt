8daba8031225b7fef67ac9387135fc3b0c063856
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-06-30 08:49:57 +0530

    Add Expo Blur for custom backdrop in FiltersModel component

--- app/(tabs)/home/image.tsx
+++ app/(tabs)/home/image.tsx
@@ -1,11 +1,14 @@
-import { StyleSheet, View, Text, TouchableOpacity, Platform } from 'react-native'
+import { StyleSheet, View, Text, TouchableOpacity, Platform, ActivityIndicator, Pressable, Alert } from 'react-native'
 import React from 'react'
 import { BlurView } from 'expo-blur'
-import { Ionicons } from '@expo/vector-icons'
+import { Entypo, Ionicons, Octicons } from '@expo/vector-icons'
 import { Stack, router, useLocalSearchParams, useRouter } from 'expo-router'
 import Colors from '@/constants/Colors'
 import { Image } from 'expo-image'
-import { wp } from '@/helpers/common'
+import { hp, wp } from '@/helpers/common'
+import * as FileSystem from 'expo-file-system'
+import * as Sharing from 'expo-sharing'
+import Toast, { ToastConfig, ToastConfigParams } from 'react-native-toast-message'
 
 
 const ImageScreen = () => {
@@ -16,13 +19,19 @@ const ImageScreen = () => {
   
   let uri = item?.webformatURL;
 
-  const [status, setStatus] = React.useState('')
+  const [status, setStatus] = React.useState('loading')
+
+  const fileName = (item?.previewURL as string)?.split('/').pop();
+  
+  const imageUrl = uri;
+
+  const filePath = `${FileSystem.documentDirectory}${fileName}`
 
   const getSize = () => {
 
     const aspectRatio = item?.imageWidth / item?.imageHeight
 
-    const maxWidth = Platform.OS == 'web' ? wp(50) : wp(92);
+    const maxWidth = Platform.OS == 'web' ? Number(wp(50)) : Number(wp(92)) as number;
     let calculateHeight = maxWidth / aspectRatio;
     let calculateWidth = maxWidth;
 
@@ -38,6 +47,66 @@ const ImageScreen = () => {
 
   const onLoad = () => {
     setStatus('')
+  }
+
+  const handleDownloadImage = async () => {
+    if(Platform.OS == 'web') {
+      const anchor = document.createElement('a');
+      anchor.href = imageUrl;
+      anchor.target = '_blank';
+      anchor.download = fileName || 'download';
+      document.body.appendChild(anchor);  
+      anchor.click();
+      document.body.removeChild(anchor);
+    } else {
+      setStatus('downloading')
+      let uri = await downloadFile()
+      if(uri) {
+        showToast({message: 'Image downloaded successfully'})
+      }
+    }
+  }
+
+  const handleShareImage = async () => {
+    if(Platform.OS == 'web') {
+      showToast({message: 'Link copied to clipboard'})
+    } else {
+      setStatus('sharing');
+      let uri = await downloadFile()
+      if(uri) {
+        await Sharing.shareAsync(uri)
+      }
+    }
+    
+  }
+
+  const downloadFile = async () => {
+    try {
+      const { uri } = await FileSystem.downloadAsync(imageUrl, filePath)
+      setStatus('');
+      console.log('Finished downloading to ', uri)
+      return uri
+    } catch (err) {
+      console.log('Error downloading file: ', err);
+      setStatus('');
+      Alert.alert('Failed to download image');
+    }
+  }
+
+  const showToast = ({message}: {message: string}) => {
+    Toast.show({
+      type: 'success',
+      position: 'bottom',
+      text1: message,
+    });
+  }
+
+  const toastConfig: ToastConfig = {
+    success: ({ text1, props, ...rest }: ToastConfigParams<any>) => (
+      <View style={styles.toast}>
+        <Text style={styles.toastTxt}>{text1}</Text>
+      </View>
+    ),
   }
   
   return (
@@ -59,13 +128,58 @@ const ImageScreen = () => {
     intensity={100}
     >
       <View style={getSize()}>
+        <View style={styles.loading}>
+          {
+            status == 'loading' && <ActivityIndicator size='large' color={Colors.primary} />
+          }
+        </View>
         <Image 
-          source={uri}
+          source={Array.isArray(uri) ? uri[0] : uri}
           transition={100}
           style={[styles.image, getSize()]}
           onLoad={onLoad}
         />
       </View>
+      <View style={styles.buttons}>
+        <View>
+          <Pressable onPress={() => router.back()} style={styles.button}>
+            <Octicons name='x' size={24} color='#fff' />
+          </Pressable>
+        </View>
+
+        <View>
+          {
+            status == 'downloading' ? (
+              <View style={styles.button}>
+                <ActivityIndicator size='small' color={Colors.primary} />
+              </View>
+            ) : (
+              <Pressable onPress={handleDownloadImage} style={styles.button}>
+                <Octicons name='download' size={24} color='#fff' />
+              </Pressable>
+            )
+          }
+          
+        </View>
+
+        <View>
+        {
+            status == 'sharing' ? (
+              <View style={styles.button}>
+                <ActivityIndicator size='small' color={Colors.primary} />
+              </View>
+            ) : (
+              <Pressable onPress={handleShareImage} style={styles.button}>
+                <Entypo name='share' size={22} color='#fff' />
+              </Pressable>
+            )
+          }
+          
+        </View>
+      </View>
+
+      <Toast config={toastConfig} visibilityTime={2500} />
+
     </BlurView>
     </>
   )
@@ -95,6 +209,46 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     backgroundColor: 'rgba(255,255,255,0.1)',
     borderColor: 'rgba(255,255,255,0.1)',
+  },
+
+  loading: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 30,
+    gap: 50
+  },
+
+  button: {
+    width: 45,
+    height: 45,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(255,255,255,0.2)'
+  },
+
+  toast: {
+    padding: 15,
+    paddingHorizontal: 30,
+    alignItems: 'center', 
+    justifyContent: 'center',
+    borderRadius: 10,
+    backgroundColor: 'rgba(255,255,255,0.15)',
+  },
+
+  toastTxt: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    letterSpacing: 1
   }
 
 })
